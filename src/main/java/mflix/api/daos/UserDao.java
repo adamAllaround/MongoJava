1cd7c15fdd8a0647bd9bb3ea86c5216e6337c2c0
Adam Stankowski <adam@allaroundjava.com>
2020-09-04 08:31:56 +0200

    Project commit

---
+++ src/main/java/mflix/api/daos/UserDao.java
@@ -0,0 +1,158 @@
+package mflix.api.daos;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoWriteException;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.InsertOneOptions;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import mflix.api.models.Session;
+import mflix.api.models.User;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+@Configuration
+public class UserDao extends AbstractMFlixDao {
+
+    private final MongoCollection<User> usersCollection;
+    private final MongoCollection<Session> sessionsCollection;
+
+    private final Logger log;
+
+    @Autowired
+    public UserDao(
+            MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+        super(mongoClient, databaseName);
+        CodecRegistry pojoCodecRegistry =
+                fromRegistries(
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
+        log = LoggerFactory.getLogger(this.getClass());
+        //TODO> Ticket: User Management - implement the necessary changes so that the sessions
+        // collection returns a Session objects instead of Document objects.
+        sessionsCollection = db.getCollection("sessions", Session.class).withCodecRegistry(pojoCodecRegistry);
+    }
+
+    /**
+     * Inserts the `user` object in the `users` collection.
+     *
+     * @param user - User object to be added
+     * @return True if successful, throw IncorrectDaoOperation otherwise
+     */
+    public boolean addUser(User user) {
+        if(usersCollection.countDocuments(Filters.eq("email", user.getEmail())) > 0) {
+            throw new IncorrectDaoOperation("Cannot do this");
+        }
+        usersCollection.withWriteConcern(WriteConcern.MAJORITY).insertOne(user);
+        return true;
+
+    }
+
+    /**
+     * Creates session using userId and jwt token.
+     *
+     * @param userId - user string identifier
+     * @param jwt    - jwt string token
+     * @return true if successful
+     */
+    public boolean createUserSession(String userId, String jwt) {
+
+        if (sessionsCollection.find(Filters.eq("user_id", userId)).iterator().hasNext()) {
+            return false;
+        }
+
+        Session session = new Session();
+        session.setJwt(jwt);
+        session.setUserId(userId);
+        sessionsCollection.insertOne(session);
+        return true;
+    }
+
+    /**
+     * Returns the User object matching the an email string value.
+     *
+     * @param email - email string to be matched.
+     * @return User object or null.
+     */
+    public User getUser(String email) {
+        return  usersCollection.find(Filters.eq("email", email)).limit(1).iterator().tryNext();
+    }
+
+    /**
+     * Given the userId, returns a Session object.
+     *
+     * @param userId - user string identifier.
+     * @return Session object or null.
+     */
+    public Session getUserSession(String userId) {
+        return sessionsCollection.find(Filters.eq("user_id", userId)).iterator().tryNext();
+    }
+
+    public boolean deleteUserSessions(String userId) {
+        sessionsCollection.deleteMany(Filters.eq("user_id", userId));
+        //TODO> Ticket: User Management - implement the delete user sessions method
+        return true;
+    }
+
+    /**
+     * Removes the user document that match the provided email.
+     *
+     * @param email - of the user to be deleted.
+     * @return true if user successfully removed
+     */
+    public boolean deleteUser(String email) {
+        sessionsCollection.deleteMany(Filters.eq("user_id", email));
+        usersCollection.deleteOne(Filters.eq("email", email));
+        return true;
+    }
+
+    /**
+     * Updates the preferences of an user identified by `email` parameter.
+     *
+     * @param email           - user to be updated email
+     * @param userPreferences - set of preferences that should be stored and replace the existing
+     *                        ones. Cannot be set to null value
+     * @return User object that just been updated.
+     */
+    public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
+        User user = usersCollection.find(Filters.eq("email", email), User.class).iterator().tryNext();
+
+        if (userPreferences == null || user == null) {
+            throw new IncorrectDaoOperation("Cannot do this");
+        }
+
+
+        Map<String, String> currentPreferences = Optional.ofNullable(user.getPreferences()).orElse(new HashMap<>());
+        for(Map.Entry<String, ?> entry : userPreferences.entrySet()) {
+            currentPreferences.put(entry.getKey(), entry.getValue().toString());
+        }
+
+        usersCollection.updateOne(Filters.eq("email", email), Updates.set("preferences", currentPreferences));
+        return true;
+    }
+}
